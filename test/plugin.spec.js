10034d1b6ebf2489b4547dd96459c208ef150285
javier.brea <javier.brea@gmail.com>
2020-11-28 12:45:25 +0100

    test(unit): Add plugin unit tests

--- test/plugin.spec.js
+++ test/plugin.spec.js
@@ -4,6 +4,20 @@ const plugin = require("../src/plugin");
 describe("plugin", () => {
   let onEventSpy;
   let sandbox;
+  let eventName;
+  let shouldSkipDueToFailFast;
+  let resetShouldSkipDueToFailFast;
+
+  const getPluginMethods = (pluginEnvironmentVarValue) => {
+    plugin(onEventSpy, {
+      env: {
+        FAIL_FAST: pluginEnvironmentVarValue,
+      },
+    });
+    eventName = onEventSpy.getCall(0).args[0];
+    shouldSkipDueToFailFast = onEventSpy.getCall(0).args[1].shouldSkipDueToFailFast;
+    resetShouldSkipDueToFailFast = onEventSpy.getCall(0).args[1].resetShouldSkipDueToFailFast;
+  };
 
   beforeEach(() => {
     sandbox = sinon.createSandbox();
@@ -14,8 +28,65 @@ describe("plugin", () => {
     sandbox.restore();
   });
 
-  it("should register plugin methods for the task event", () => {
-    plugin(onEventSpy);
-    expect(onEventSpy.getCall(0).args[0]).toEqual("task");
+  describe("when initialized", () => {
+    it("should register plugin methods for the task event", () => {
+      getPluginMethods();
+      expect(eventName).toEqual("task");
+    });
+  });
+
+  describe("when it is disabled", () => {
+    it("should return shouldSkip flag as false by default", () => {
+      getPluginMethods();
+      expect(shouldSkipDueToFailFast()).toEqual(false);
+    });
+
+    it("should return shouldSkip flag as false even after setting it as true", () => {
+      getPluginMethods();
+      shouldSkipDueToFailFast(true);
+      expect(shouldSkipDueToFailFast()).toEqual(false);
+    });
+  });
+
+  describe("when it is enabled using boolean", () => {
+    it("should return shouldSkip flag as false by default when plugin ", () => {
+      getPluginMethods(true);
+      expect(shouldSkipDueToFailFast()).toEqual(false);
+    });
+
+    it("should return shouldSkip flag as true after setting it as true", () => {
+      getPluginMethods(true);
+      shouldSkipDueToFailFast(true);
+      expect(shouldSkipDueToFailFast()).toEqual(true);
+    });
+
+    it("should reset shouldSkip flag after calling to reset method", () => {
+      getPluginMethods(true);
+      shouldSkipDueToFailFast(true);
+      expect(shouldSkipDueToFailFast()).toEqual(true);
+      resetShouldSkipDueToFailFast();
+      expect(shouldSkipDueToFailFast()).toEqual(false);
+    });
+  });
+
+  describe("when it is enabled using string", () => {
+    it("should return shouldSkip flag as false by default when plugin ", () => {
+      getPluginMethods("true");
+      expect(shouldSkipDueToFailFast()).toEqual(false);
+    });
+
+    it("should return shouldSkip flag as true after setting it as true", () => {
+      getPluginMethods("true");
+      shouldSkipDueToFailFast(true);
+      expect(shouldSkipDueToFailFast()).toEqual(true);
+    });
+
+    it("should reset shouldSkip flag after calling to reset method", () => {
+      getPluginMethods("true");
+      shouldSkipDueToFailFast(true);
+      expect(shouldSkipDueToFailFast()).toEqual(true);
+      resetShouldSkipDueToFailFast();
+      expect(shouldSkipDueToFailFast()).toEqual(false);
+    });
   });
 });
