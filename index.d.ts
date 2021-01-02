f3defb4d88c832c636f76da5a632058ea0e45594
javier.brea <javier.brea@gmail.com>
2021-01-02 18:41:22 +0100

    feat: Add TypeScript declarations for custom config (#27)

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface FailFastConfigOptions {
+    /**
+     * Disables fail-fast plugin
+     * If the test fails, the rest of tests won't be skipped
+    */
+    enabled: boolean
+  }
+
+  interface TestConfigOverrides extends Partial<TestConfigOverrides> {
+    /**
+     * Configuration for fail-fast plugin
+    */
+    failFast: Partial<FailFastConfigOptions>
+  }
+}
