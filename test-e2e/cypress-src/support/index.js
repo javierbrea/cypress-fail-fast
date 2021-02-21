a1f09bf0df392f27cd5491aa18711c965d5092e1
javier.brea <javier.brea@gmail.com>
2021-02-21 09:25:45 +0100

    test(e2e): Check that test:after:run event is executed in failed tests (#61)

--- test-e2e/cypress-src/support/index.js
+++ test-e2e/cypress-src/support/index.js
@@ -1,1 +1,9 @@
+/* global Cypress */
+
 import "cypress-fail-fast";
+
+Cypress.on("test:after:run", (test) => {
+  if (test.state === "failed") {
+    cy.task("log", "Executing test:after:run event in failed test");
+  }
+});
--- test-e2e/cypress-src/support/index.ts
+++ test-e2e/cypress-src/support/index.ts
@@ -1,1 +1,7 @@
 import "./cypress-fail-fast";
+
+Cypress.on("test:after:run", (test, runnable) => {
+  if (test.state === "failed") {
+    cy.task("log", "Log after failed test");
+  }
+});
