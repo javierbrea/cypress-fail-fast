01b2edb82690c4f5894e0e52045699d24c1d8c79
javier.brea <javier.brea@gmail.com>
2021-04-02 09:28:30 +0200

    test: Add parallel callbacks unit tests

--- test/plugin.spec.js
+++ test/plugin.spec.js
@@ -8,8 +8,8 @@ describe("plugin", () => {
   let failFastShouldSkip;
   let failFastResetSkip;
 
-  const getPluginMethods = () => {
-    plugin(onEventSpy);
+  const getPluginMethods = (config) => {
+    plugin(onEventSpy, {}, config);
     eventName = onEventSpy.getCall(0).args[0];
     failFastShouldSkip = onEventSpy.getCall(0).args[1].failFastShouldSkip;
     failFastResetSkip = onEventSpy.getCall(0).args[1].failFastResetSkip;
@@ -45,7 +45,7 @@ describe("plugin", () => {
   });
 
   describe("when it is enabled using boolean", () => {
-    it("should return shouldSkip flag as false by default when plugin ", () => {
+    it("should return shouldSkip flag as false by default", () => {
       getPluginMethods();
       expect(failFastShouldSkip()).toEqual(false);
     });
@@ -66,7 +66,7 @@ describe("plugin", () => {
   });
 
   describe("when it is enabled using string", () => {
-    it("should return shouldSkip flag as false by default when plugin ", () => {
+    it("should return shouldSkip flag as false by default", () => {
       getPluginMethods();
       expect(failFastShouldSkip()).toEqual(false);
     });
@@ -83,6 +83,59 @@ describe("plugin", () => {
       expect(failFastShouldSkip()).toEqual(true);
       failFastResetSkip();
       expect(failFastShouldSkip()).toEqual(false);
+    });
+  });
+
+  describe("when parallel callbacks are provided", () => {
+    it("should return shouldSkip flag as true if flag is false but isCancelled callback returns true", () => {
+      getPluginMethods({
+        parallelCallbacks: {
+          isCancelled: () => true,
+        },
+      });
+      expect(failFastShouldSkip()).toEqual(true);
+    });
+
+    it("should return shouldSkip flag as false if flag is false and isCancelled callback does not return value", () => {
+      getPluginMethods({
+        parallelCallbacks: {
+          isCancelled: () => null,
+        },
+      });
+      expect(failFastShouldSkip()).toEqual(false);
+    });
+
+    it("should return shouldSkip flag as true if flag is true even when isCancelled callback returns false", () => {
+      getPluginMethods({
+        parallelCallbacks: {
+          isCancelled: () => false,
+        },
+      });
+      failFastShouldSkip(true);
+      expect(failFastShouldSkip()).toEqual(true);
+    });
+
+    it("should call to onCancel callback when shouldSkip flag is set to true", () => {
+      const spy = sandbox.spy();
+      getPluginMethods({
+        parallelCallbacks: {
+          onCancel: spy,
+        },
+      });
+      failFastShouldSkip(true);
+      expect(spy.callCount).toEqual(1);
+    });
+
+    it("should not call to onCancel callback when shouldSkip flag is set with a value different to true", () => {
+      const spy = sandbox.spy();
+      getPluginMethods({
+        parallelCallbacks: {
+          onCancel: spy,
+        },
+      });
+      failFastShouldSkip();
+      failFastShouldSkip(false);
+      expect(spy.callCount).toEqual(0);
     });
   });
 });
