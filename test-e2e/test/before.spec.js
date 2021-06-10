957446c03dfd341487dbb2fc2dc1e4bb4f28dab1
javierbrea <javier.brea@gmail.com>
2021-06-10 07:20:30 +0200

    test(#91): Add hooks e2e tests using spec strategy

--- test-e2e/test/before.spec.js
+++ test-e2e/test/before.spec.js
@@ -29,3 +29,34 @@ runSpecsTests("When before hook fails", {
     CYPRESS_FAIL_FAST_STRATEGY: "run",
   },
 });
+
+runSpecsTests("When before hook fails in spec mode", {
+  skipVariants: true,
+  specs: "before-failing",
+  specsResults: [
+    {
+      logBefore: true,
+      executed: 4,
+      passed: 0,
+      failed: 0,
+      skipped: 4,
+    },
+    {
+      logBefore: true,
+      executed: 4,
+      passed: 4,
+      failed: 0,
+      skipped: 0,
+    },
+    {
+      logBefore: true,
+      executed: 4,
+      passed: 4,
+      failed: 0,
+      skipped: 0,
+    },
+  ],
+  env: {
+    CYPRESS_FAIL_FAST_STRATEGY: "spec",
+  },
+});
