c9652b81e9c9238455c888a8fc6f5c38a898b8eb
javierbrea <javier.brea@gmail.com>
2022-08-16 09:27:24 +0200

    test(#235): Use Cypress 9

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  e2e: {
-    baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {
-      require("cypress-fail-fast/plugin")(on, config);
-      return config;
-    },
-    specPattern: [
-      "cypress/e2e/*.ts",
-    ],
-  },
-  video: false,
+  baseUrl: "http://localhost:3000",
+  video: false
 };
