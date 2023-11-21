bd0d0523375ed11036fb6d82f2af53ac3c10f480
javierbrea <javier.brea@gmail.com>
2023-11-21 18:59:42 +0100

    chore: Run all E2E tests

--- test-e2e/jest.config.js
+++ test-e2e/jest.config.js
@@ -16,5 +16,5 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: ["**/test/**/*.spec.js"],
-  testMatch: ["**/test/**/before.spec.js"],
+  // testMatch: ["**/test/**/parallel.spec.js"],
 };
